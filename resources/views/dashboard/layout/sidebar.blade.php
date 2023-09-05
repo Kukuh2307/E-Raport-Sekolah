08b17489dd6c2ab7eda08b96835bb46646706b75
Kukuh2307 <kukukhagung12@gmail.com>
2023-09-05 21:19:42 +0700

    (add) tampilan dashboard awal

---
+++ resources/views/dashboard/layout/sidebar.blade.php
@@ -0,0 +1,47 @@
+<aside class="left-sidebar">
+    <!-- Sidebar scroll-->
+    <div class="scroll-sidebar">
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav">
+            <ul id="sidebarnav">
+                <li> <a class="waves-effect waves-dark" href="index.html" aria-expanded="false"><i
+                            class="fa fa-tachometer"></i><span class="hide-menu">Dashboard</span></a>
+                </li>
+                <li> <a class="waves-effect waves-dark" href="pages-profile.html" aria-expanded="false"><i class="fa fa-graduation-cap"></i><span class="hide-menu">Profile Sekolah</span></a>
+                </li>
+                <li> <a class="waves-effect waves-dark" href="table-basic.html" aria-expanded="false"><i
+                            class="fa fa-location-arrow"></i><span class="hide-menu">Kelas</span></a>
+                </li>
+                <li> <a class="waves-effect waves-dark" href="icon-fontawesome.html" aria-expanded="false"><i
+                            class="fa fa-users"></i><span class="hide-menu">Guru</span></a>
+                </li>
+                <p class="ps-3 fw-bold">Indikator</p>
+                <li> <a class="waves-effect waves-dark" href="map-google.html" aria-expanded="false"><i
+                            class="fa fa-star"></i><span class="hide-menu">NAM</span></a>
+                </li>
+                <li> <a class="waves-effect waves-dark" href="map-google.html" aria-expanded="false"><i
+                            class="fa fa-star"></i><span class="hide-menu">Kognitif</span></a>
+                </li>
+                <li> <a class="waves-effect waves-dark" href="map-google.html" aria-expanded="false"><i
+                            class="fa fa-star"></i><span class="hide-menu">Seni</span></a>
+                </li>
+                <li> <a class="waves-effect waves-dark" href="map-google.html" aria-expanded="false"><i
+                            class="fa fa-star"></i><span class="hide-menu">Sosem</span></a>
+                </li>
+                <li> <a class="waves-effect waves-dark" href="map-google.html" aria-expanded="false"><i
+                            class="fa fa-star"></i><span class="hide-menu">Bahasa</span></a>
+                </li>
+                <li> <a class="waves-effect waves-dark" href="map-google.html" aria-expanded="false"><i
+                            class="fa fa-star"></i><span class="hide-menu">Fisik Motorik</span></a>
+                </li>
+                
+            </ul>
+            {{-- <div class="text-center mt-4">
+                <a href="https://www.wrappixel.com/templates/adminwrap/"
+                    class="btn waves-effect waves-light btn-info hidden-md-down text-white"> Upgrade to Pro</a>
+            </div> --}}
+        </nav>
+        <!-- End Sidebar navigation -->
+    </div>
+    <!-- End Sidebar scroll-->
+</aside>
